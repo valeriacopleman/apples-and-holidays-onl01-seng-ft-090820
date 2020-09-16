00e78b0ada44d70df9c1e5d295810ca63aa7f8c5
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 22:33:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
 holiday_hash.each do |seasons, holidays|
+  puts "#{seasons.to_s.capitalize}:"
   holidays.each do |holidays, supplies|
       puts "#{seasons.to_s.capitalize}:"
       puts "#{holidays.to_s.split("_").map(&:capitalize).join(" " )}"
